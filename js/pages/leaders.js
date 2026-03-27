453105e1fc6c707aa01b2d0f475da3367e864451
TimSmokovski <TimSmokovski@users.noreply.github.com>
2026-03-27 03:39:59 +0000

    deploy: 04b76d25fe9b2d19caffaaba544362f314c3a767

--- js/pages/leaders.js
+++ js/pages/leaders.js
@@ -64,7 +64,7 @@ function renderTop3Item(leader, rank) {
       <div class="top3-rank">${medals[rank]}</div>
       <div class="top3-avatar">${_avatarHtml(leader, 48)}</div>
       <div class="top3-name">${leader.name}</div>
-      <div class="top3-stars">${stars}</div>
+      <div class="top3-stars">${_goldStar(11)} ${stars}</div>
       ${leader.prize ? `<div style="font-size:20px;margin-top:6px">${leader.prize}</div>` : ''}
     </div>
   `;
@@ -83,7 +83,7 @@ function renderLeaderItem(leader) {
       <div class="leader-avatar">${_avatarHtml(leader, 38)}</div>
       <div class="leader-info">
         <div class="leader-name">${leader.name}</div>
-        <div class="leader-stars">${stars}</div>
+        <div class="leader-stars">${_goldStar(12)} ${stars}</div>
       </div>
       <div class="leader-prize">${leader.prize || ''}</div>
     </div>
